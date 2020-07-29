33dc486bd943cc2e3bb293f07eef412820367658
Mike Sapienza <mikesapaudio@gmail.com>
2020-07-29 18:17:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,14 +10,17 @@ end
 
 def division(num1, num2)
 sum = num2 / num1
+sum
 end
 
 def multiplication(num1, num2)
-num1 * num2
+sum = num1 * num2
+sum
 end
 
 def modulo(num1, num2)
-num1 $ num2
+rem = num1 $ num2
+rem
 end
 
 def square_root(num)
